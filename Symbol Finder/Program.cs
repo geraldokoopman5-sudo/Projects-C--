326666322f9a6dd75866d41680d80ca6281c2e34
geraldokoopman5-sudo <geraldokoopman5@gmail.com>
2026-02-19 10:35:44 +0200

    Add files via upload

---
+++ Symbol Finder/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int guess;
+        bool run = false;
+        string con;
+        while (!run)
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(1, 2);
+            Console.WriteLine("Guess a number between 1 and 50");
+            guess = int.Parse(Console.ReadLine());
+            if (guess >= num )
+            {
+                Console.WriteLine("Number is too high");
+            }
+            else if (guess <= num) 
+            { 
+                Console.WriteLine("Number is too low");
+            }
+           
+            if (guess == num)
+            {
+                Console.WriteLine("Correct number");
+                Console.WriteLine("Would you like to continue y/n");
+                con = Console.ReadLine().ToLower();
+            }
+          
+            if (con == "n")
+            {
+                run = true;
+            }
+
+        }
+    }
+}   
